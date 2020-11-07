3a22a819c818914978f9bb65aebe0dd50f7c55dd
dodgez <zacharysdodge@gmail.com>
2020-11-06 22:09:09 -0800

    Refactor game state

--- app/Main.hs
+++ app/Main.hs
@@ -1,42 +1,6 @@
 module Main where
 
-import Control.Concurrent (threadDelay)
-import Data.IORef
-import Graphics.Rendering.OpenGL
-import Graphics.UI.GLUT
-  ( ActionOnWindowClose (ContinueExecution),
-    DisplayMode (RGBMode, WithDepthBuffer),
-    actionOnWindowClose,
-    closeCallback,
-    createWindow,
-    displayCallback,
-    exit,
-    initialDisplayMode,
-    initialize,
-    mainLoopEvent,
-    windowSize,
-  )
 import Lib
 
-mainLoop timer run = do
-  threadDelay 10000
-  paint timer
-  modifyIORef timer (\time -> time + 1)
-  mainLoopEvent
-  shouldRun <- readIORef run
-  if shouldRun then mainLoop timer run else return ()
-
 main :: IO ()
-main = do
-  timer <- newIORef 0
-  run <- newIORef True
-  initialize "something" []
-  actionOnWindowClose $= ContinueExecution
-  initialDisplayMode $= [RGBMode, WithDepthBuffer]
-  _ <- createWindow "test"
-  windowSize $= Size 1440 900
-  clearColor $= (Color4 (0 :: GLfloat) (0 :: GLfloat) (0 :: GLfloat) (1 :: GLfloat))
-  displayCallback $= (paint timer)
-  closeCallback $= Just (close run)
-  mainLoop timer run
-  exit
+main = setupAndRun
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,26 +1,33 @@
-module Lib (
-  render,
-  paint,
-  close
-) where
+module Lib where
 
+import Control.Concurrent
+import Data.IORef
+import qualified Data.Vector as V
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
-import Data.IORef
 
-render :: Integer -> IO ()
-render time = do
+screenWidth = 1440
+screenHeight = 900
+
+screenToGLX :: GLfloat -> GLfloat
+screenToGLX x = screenToGL x screenWidth
+
+screenToGLY :: GLfloat -> GLfloat
+screenToGLY y = screenToGL y screenHeight
+
+screenToGL :: GLfloat -> GLsizei -> GLfloat
+screenToGL x max = 2*x/(fromIntegral max) - 1
+
+render :: V.Vector (GLfloat, GLfloat) -> IO ()
+render gameState = do
   color (Color3 (0 :: GLfloat) (1 :: GLfloat) (1 :: GLfloat))
-  vertex (Vertex2 (0 :: GLfloat) (2*(fromIntegral (time `mod` 500) / 500)-1 :: GLfloat))
-  vertex (Vertex2 (-1 :: GLfloat) (-1 :: GLfloat))
-  vertex (Vertex2 (1 :: GLfloat) (-1 :: GLfloat))
+  mapM_ (\v -> vertex (Vertex2 (fst v) (snd v))) gameState
 
-paint :: IORef Integer -> DisplayCallback
-paint timer = do
-  time <- readIORef timer
-  if time `mod` 100 == 0 then putStrLn $ "Hello from print " ++ show time else return ()
+paint :: IORef (V.Vector (GLfloat, GLfloat)) -> DisplayCallback
+paint gameState = do
+  curGameState <- readIORef gameState
   clear [ColorBuffer, DepthBuffer]
-  renderPrimitive Triangles (render time)
+  renderPrimitive Points (render curGameState)
   flush
   swapBuffers
   return ()
@@ -30,3 +37,37 @@ close run = do
   putStrLn "Closing window"
   writeIORef run False
   return ()
+
+wrap :: Ord a => a -> a -> a -> a
+wrap min max x = if x < min then max else if x > max then min else x
+
+clamp :: Ord a => a -> a -> a -> a
+clamp min max x = if x < min then min else if x > max then max else x
+
+update :: V.Vector (GLfloat, GLfloat) -> V.Vector (GLfloat, GLfloat)
+update particles = V.map (\part -> (fst part, wrap (-1) 1 $ 0.01*((fst part+1)/2) + snd part)) particles
+
+setupAndRun :: IO ()
+setupAndRun = do
+  gameState <- newIORef $ V.fromList [(screenToGLX x, if x / 2 == (fromIntegral $ round (x / 2)) then 0.1 else 0) | x <- [0..fromIntegral screenWidth]]
+  running <- newIORef True
+  initialize "something" []
+  actionOnWindowClose $= ContinueExecution
+  initialDisplayMode $= [RGBMode, WithDepthBuffer]
+  _ <- createWindow "test"
+  windowSize $= Size screenWidth screenHeight
+  clearColor $= (Color4 (0 :: GLfloat) (0 :: GLfloat) (0 :: GLfloat) (1 :: GLfloat))
+  displayCallback $= (paint gameState)
+  closeCallback $= Just (close running)
+  loop gameState running
+  exit
+
+loop :: IORef (V.Vector (GLfloat, GLfloat)) -> IORef Bool -> IO ()
+loop gameState run = do
+  threadDelay 16666
+  curGameState <- readIORef gameState
+  writeIORef gameState $ update curGameState
+  paint gameState
+  mainLoopEvent
+  shouldRun <- readIORef run
+  if shouldRun then loop gameState run else return ()
